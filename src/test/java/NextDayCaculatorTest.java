e4aec370d7862c37d0f33a1f99daf89ca4ec30f4
longhoang94 <longsutls@gmail.com>
2018-08-16 19:43:33 +0700

    Pass test case: Last day of January 2018

--- src/test/java/NextDayCaculatorTest.java
+++ src/test/java/NextDayCaculatorTest.java
@@ -6,10 +6,20 @@ import static org.junit.Assert.assertArrayEquals;
 
 public class NextDayCaculatorTest {
     @Test
-    @DisplayName("Test day")
-    public void testNextDayCaculator() {
+    @DisplayName("Test the first day of January, 2018")
+    public void testFirstDayOfJanuary() {
         int[] time = {1, 1, 2018};
         int[] expected = {2, 1, 2018};
+
+        int[] actual = NextDayCaculator.findNextDay(time);
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("Test the last day of January, 2018")
+    public void testLastDayOfJanuary() {
+        int[] time = {31, 1, 2018};
+        int[] expected = {1, 2, 2018};
 
         int[] actual = NextDayCaculator.findNextDay(time);
         assertArrayEquals(expected, actual);
